75c319c8796226f8c1795e7ab8fcb7a991aab030
Anurag Jadhav <135056153+anu2225@users.noreply.github.com>
2024-05-08 18:21:12 +0530

    Update CC-2.py

--- CC-2.py
+++ CC-2.py
@@ -1,13 +1,2 @@
 print('Hello')
 
-
-
-Step For CC 2nd practical 
-Google app Engine 
-1) login google app engine
-2) click dashboard
-3) create new project 
-4) go to search box and type Admin API 
-5) then open api and enable this option 
-6) then open cloud shell and type git clone and paste path github code 
-7) python and type file name.py and then run file print hello word 
